9c32590fc228aa8f03d6e4791fa30365699d4d05
amirgh82 <amirghhastam@gmail.com>
2023-05-22 23:03:03 +0330

    Writing the html structure in the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,50 @@ import React from 'react'
 export default function Header() {
     return (
         <header className='header'>
-            
+            <div className="container">
+                <nav className='nav'>
+                    <h1 className="nav__logo">
+                        WA.
+                    </h1>
+
+                    <ul className="menu">
+                        <li className="menu__item">
+                            <a href="#" className="menu__link menu__link-active">
+                                Home
+                            </a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">
+                                About us
+                            </a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">
+                                Resume
+                            </a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">
+                                Services
+                            </a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">
+                                Portfolio
+                            </a>
+                        </li>
+                        <li className="menu__item">
+                            <a href="#" className="menu__link">
+                                Pricing
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div className="nav__toggle-icon">
+                        <div className="nav__toggle-line"></div>
+                    </div>
+                </nav>
+            </div>
         </header>
     )
 }
