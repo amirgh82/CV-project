499641d69e5f0a210f2b461575f675b05a7a947e
amirgh82 <amirghhastam@gmail.com>
2023-05-23 19:29:51 +0330

    Completion of the navbar section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,52 +11,55 @@ export default function Header() {
 
 
     return (
-        <header className='header'>
-            <div className="container">
-                <nav className='nav'>
-                    <h1 className="nav__logo">
-                        WA.
-                    </h1>
+        <>
+            <header className='header'>
+                <div className="container">
+                    <nav className='nav'>
+                        <h1 className="nav__logo">
+                            WA.
+                        </h1>
 
-                    <ul className={`menu ${isChangeBurger ? 'menu--open' : ''} `}>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link menu__link-active">
-                                Home
-                            </a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">
-                                About us
-                            </a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">
-                                Resume
-                            </a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">
-                                Services
-                            </a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">
-                                Portfolio
-                            </a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">
-                                Pricing
-                            </a>
-                        </li>
-                    </ul>
+                        <ul className={`menu ${isChangeBurger ? 'menu--open' : ''} `}>
+                            <li className="menu__item">
+                                <a href="#" className="menu__link menu__link-active">
+                                    Home
+                                </a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#" className="menu__link">
+                                    About us
+                                </a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#" className="menu__link">
+                                    Resume
+                                </a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#" className="menu__link">
+                                    Services
+                                </a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#" className="menu__link">
+                                    Portfolio
+                                </a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="#" className="menu__link">
+                                    Pricing
+                                </a>
+                            </li>
+                        </ul>
 
-                    <div className={`nav__toggle-icon ${isChangeBurger ? 'nav__toggle-icon--open' : ''} `} onClick={changeToggle}>
-                        <div className="nav__toggle-line"></div>
-                    </div>
-                </nav>
-            </div>
-        </header>
+                        <div className={`nav__toggle-icon ${isChangeBurger ? 'nav__toggle-icon--open' : ''} `} onClick={changeToggle}>
+                            <div className="nav__toggle-line"></div>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <div className={`cover ${isChangeBurger ? 'cover-show' : ''}`}></div>
+        </>
     )
 }
 
