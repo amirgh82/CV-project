37efe68ab2f12bbbbbbadecbcbfb12fe58156692
amirgh82 <amirghhastam@gmail.com>
2023-05-30 15:27:30 +0330

    complate Aboute text header

--- src/components/Dependencies/Titles/Title.jsx
+++ src/components/Dependencies/Titles/Title.jsx
@@ -5,7 +5,7 @@ export default function Title({ children }) {
         <div className="section-head">
             <h3 className="section-head-title">{children[0]}</h3>
             <p className="section-head-caption">{children[1]}</p>
-            <span className="section-head-line">-head</span>
+            <span className="section-head-line"></span>
         </div>
     )
 }
