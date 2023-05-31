b5bd03abb0e1ebde5f614453407d7b60d1a8c98b
amirgh82 <amirghhastam@gmail.com>
2023-06-01 01:05:22 +0330

    write Html Resume component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navbar from './components/Navbar/Navbar'
 import Main from './components/Page/Main';
 import About from './components/About/About';
 import Services from './components/Services/Services';
+import Resume from './components/Resume/Resume'
 function App() {
   return (
     <>
       <Navbar />
       <Main />
       <About />
-      <Services/>
+      <Services />
+      <Resume />
     </>
   );
 }
