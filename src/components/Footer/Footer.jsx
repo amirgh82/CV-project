9dcdf1f723561faae543a0056da11b251d0f66e6
amirgh82 <amirghhastam@gmail.com>
2023-06-05 21:17:58 +0330

    write Html Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 import './Footer.css'
+import { BiLogoFacebookCircle, BiLogoPinterest } from 'react-icons/bi'
+import { TbBrandWordpress } from 'react-icons/tb'
+import { FaVolleyball } from 'react-icons/fa'
+import { AiOutlineSkype } from 'react-icons/ai'
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className='footer'>
+            <div className="container">
+                <div className="footer__wrapper">
+                    <p className="footer__copy-right">© 2019 createuiux. All rights reserved.</p>
+                    <div className="footer__socials">
+                        <a href="#" className="footer__social-link"></a>
+                        <a href="#" className="footer__social-link"><BiLogoFacebookCircle /></a>
+                        <a href="#" className="footer__social-link"><TbBrandWordpress /></a>
+                        <a href="#" className="footer__social-link"><FaVolleyball /></a>
+                        <a href="#" className="footer__social-link"><BiLogoPinterest /></a>
+                        <a href="#" className="footer__social-link"><AiOutlineSkype /></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
 }
